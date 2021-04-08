4612f14b418d639221843215ea33b5f5da47a642
Alejandro <alejandro.mancilla.umsa@gmail.com>
2021-04-08 14:35:33 -0400

    conectando a firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,35 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:web_taxis/Pages/HomePage.dart';
  
-void main() => runApp(MyApp());
- 
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
+  // Cree el futuro de inicialización fuera de `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+    return FutureBuilder(
+      // Inicializar FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Compruebe si hay errores
+        if (snapshot.hasError) {
+          return CircularProgressIndicator();
+        }
+        // Una vez completado, muestre su solicitud
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'Material App',
+            home: HomePage()
+          );
+        }
+        // De lo contrario, muestre algo mientras espera a que se complete la inicialización
+        return CircularProgressIndicator();
+      },
     );
   }
 }
